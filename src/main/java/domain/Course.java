5c3f1f06fc26f1e74b418e5458dbd639556a7f2a
huang <aa769850020@outlook.com>
2021-05-25 21:34:45 +0800

    Update getCourses

---
+++ src/main/java/domain/Course.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class Course {
+    private String courseID;
+    private String courseName;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+}
